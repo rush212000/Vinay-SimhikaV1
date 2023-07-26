e1b5235fd0910fa32ba9a5d0a4c742a7ac38a340
rush212000 <57355106+rush212000@users.noreply.github.com>
2023-07-27 00:03:51 +0100

    RSVP

--- src/pages/RSVP.js
+++ src/pages/RSVP.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import VSfoot from '../img/RSVP/V&SFOOT.png';
 
 const RSVP = () => {
+
+  const Footer = () => {
+    return (
+      <footer className="footer mt-auto">
+        <div className="container mx-auto py-4">
+          {/* Add your footer content here */}
+          <div className="flex justify-center items-center">
+            <img src={VSfoot} alt="Footer Image" className="w-20 h-20 mr-4" />
+            <p className="text-center text-gray-600">
+              <span>© {new Date().getFullYear()}</span> <span className="ml-auto"></span>
+            </p>
+          </div>
+        </div>
+      </footer>
+    );
+  };
+
   return (
     <div style={{ backgroundColor: '#F6F6F6' }}>
-     
+      <nav className="navbar" style={{ backgroundColor: 'black', color: 'white' }}>
+        {/* Add your navbar content here */}
+        <h1>Navbar Content</h1>
+      </nav>
       <div>
         <h1></h1>
         <iframe
@@ -11,9 +32,10 @@ const RSVP = () => {
           title="Zola RSVP"
           width="100%"
           height="900"
-          top= "-25vh"
+          top="-25vh"
         ></iframe>
       </div>
+      <Footer />
     </div>
   );
 };
