4533403bcd55791f36a3bb77fad7e245dc9199c9
vinaybhudia <140613982+vinaybhudia@users.noreply.github.com>
2023-07-27 14:10:19 +0100

    Our Story Text

--- src/pages/OurStory.js
+++ src/pages/OurStory.js
@@ -37,32 +37,6 @@ const OurStory = () => {
         </h1>
       </div>
 
-      <div
-        style={{
-          height: '200vh',
-          background: '#E0E0E0',
-          display: 'flex',
-          flexDirection: 'column', // Change flexDirection to column
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <h2
-          style={{
-            textAlign: 'center',
-            fontFamily: 'shelby, sans-serif',
-            fontStyle: 'normal',
-            fontSize: '69px',
-            color: '#333',
-          }}
-        >
-          <br />
-          <br />
-          <br />
-          How We Met
-        </h2>
-
-
         <div
       className='container mx-auto h-full flex flex-col'
         style={{
@@ -87,7 +61,7 @@ const OurStory = () => {
         >
            
 
-          Vinay add text here!
+           How We Met
         </h2>
         <div 
         style={{ marginTop: '20px', color: 'black',
@@ -101,20 +75,175 @@ const OurStory = () => {
           {/* Add more img elements here as needed */}
           {/* Content */}
             
-            Tuseday 2nd April 2024
-<br/>
-            <br />
-            Time Square Club, Bhuj
-            <br />
-            <br/>
-            We kick of the wedding shenanigans with the mehndi party where we invite you to adorn your hands with the traditional art of mehndi, or henna. Immerse yourself in the ambiance of uplifting music, the aromatic scent of mehndi and dance performances.
-            <br/>
-            <br/>
-            Dress code: Bright and vibrant outfits 
-            <br/>
-          
-          
+          Vinay would like to say we met at a photography convention but really, 
+          he slid in my DM’s after seeing me at Northolt garba in the queue for 
+          Dosa (because food over boys).
+          <br/>
+    
         </div>
+
+        <br/>
+        <br/>
+        <br/>
+
+        <h3
+        style={{
+            textAlign: 'center',
+            fontFamily: 'shelby, sans-serif',
+            fontStyle: 'normal',
+            fontSize: '75px',
+            color: '#000000',
+          }}
+        >
+        
+        How We Actually Met
+          </h3>
+<div 
+        style={{ marginTop: '20px', color: 'black',
+              fontWeight: 'regular 400',
+              fontFamily: 'cormorant infant',
+              fontStyle: 'regular',
+              textAlign: 'center',
+              fontSize: '20px'
+    }}>
+
+          {/* Add more img elements here as needed */}
+          {/* Content */}
+            
+          On 3rd December 2016, Vinay was almost involved in a drug deal before 
+          meeting me at Liverpool Street station for the Lens Bible photography 
+          convention (you can ask him more about this). On this day, he impressed 
+          me by taking a picture of an armchair (see picture of said armchair below). 
+          As you can tell, I was easily impressed. Like the gentleman he is, Vinay 
+          left me in one of the seminars by myself while he went off to have his own 
+          private shoot with one of the photographers attending the convention. 
+          Despite being the first day we had ever met, it felt as though we had 
+          known each other for years. From this day on, we spoke every single day. 
+          As cheesy as it sounds, you could say, just like a camera, ‘we just clicked.’
+          <br/>
+    
+        </div>
+        
+        <br/>
+        <br/>
+        <br/>
+
+
+        <h3
+        style={{
+            textAlign: 'center',
+            fontFamily: 'shelby, sans-serif',
+            fontStyle: 'normal',
+            fontSize: '75px',
+            color: '#000000',
+          }}
+        >
+        
+        Will You Be My Girlfriend?
+          </h3>
+<div 
+        style={{ marginTop: '20px', color: 'black',
+              fontWeight: 'regular 400',
+              fontFamily: 'cormorant infant',
+              fontStyle: 'regular',
+              textAlign: 'center',
+              fontSize: '20px'
+    }}>
+
+          {/* Add more img elements here as needed */}
+          {/* Content */}
+            
+          Vinay originally planned to ask me to be his girlfriend on 10th March 2017. 
+          We ended up doing 4 different date activities on that day because after 
+          each activity, he was too scared to ask me out. The day started with Vinay 
+          picking me up from my house in Willesden, going to Dinosaur Golf in Northolt, 
+          followed by Topgolf in Watford. We then stopped for dinner at Pizza Express in 
+          Watford Atria followed by the arcades in Acton. By the time he finallyyyy plucked 
+          up the courage to ask me to be his girlfriend, it was the next day 11th March 2017 
+          and it was outside the picturesque location of Willesden Sports Centre in Vinay’s 
+          Volkswagen Polo (famously known as V11NUB)…
+          <br/>
+    
+        </div>
+        
+        <br/>
+        <br/>
+        <br/>
+
+
+        <h3
+        style={{
+            textAlign: 'center',
+            fontFamily: 'shelby, sans-serif',
+            fontStyle: 'normal',
+            fontSize: '75px',
+            color: '#000000',
+          }}
+        >
+        
+        The Proposal
+          </h3>
+<div 
+        style={{ marginTop: '20px', color: 'black',
+              fontWeight: 'regular 400',
+              fontFamily: 'cormorant infant',
+              fontStyle: 'regular',
+              textAlign: 'center',
+              fontSize: '20px'
+    }}>
+
+          {/* Add more img elements here as needed */}
+          {/* Content */}
+            
+          Fast forward five years to 2022, the time came for us to get engaged. 
+          Our parents had given their blessing and we set a date for our Indian 
+          engagement (Chandlo) in October 2022, giving me a small window of 
+          opportunity to propose. With only two weeks left until the Chandlo, 
+          Simhika knew I would propose on one of these two weekends. 
+          <br/>
+          <br/>
+          Knowing this, I came up with a plan to convince Simhika that we were 
+          going to go on holiday on both weekends. The trip to Dubrovnik, Croatia 
+          had been planned and we set off for the airport. At Luton airport, I got 
+          stopped at security and was made to open up my bag where I had cleverly 
+          hidden the ring in my pooja. As my bags were about to get searched and 
+          Simhika refused to leave my side, I was now panicking that my whole plan 
+          would fall apart before we even landed in Dubrovnik. Much to my delight, 
+          my bag got rescanned without ever having to be opened.
+          <br/>
+          <br/>
+          The plan then nearly fell apart once again when we landed in Dubrovnik. 
+          It was late in the evening and we were both tired on the coach to our 
+          accommodation. Simhika asked me if I would post anything from the trip 
+          on social media, to which Ireplied without thinking “of course I am, I’m 
+          going to post the proposal.” 
+          <br/>
+          <br/>
+          Now comes the day of the proposal where I had hired a photographer for a 
+          two-hour photoshoot. Simhika found this slightly suspicious, thinking I 
+          would propose within these two hours but the photographer completed the 
+          shoot and we parted ways. Still no proposal. 
+          <br/>
+          <br/>
+          That evening, I lured Simhika out of the house with a promise of a fancy 
+          dinner date at Dubrovnik’s highest view point, Mount Srd. Here, we stopped 
+          to take in the breath-taking views of Dubrovnik’s Old Town. Simhika turned 
+          around for a split second and saw what looked like the photographer from 
+          the morning photo session. By the time she realised what was happening, 
+          I was down on one knee and popped the question! Of course, she said YES!
+          <br/>
+    
+        </div>
+        
+        <br/>
+        <br/>
+        <br/>
+
+
+
+
+
+
 
 
 
@@ -140,7 +269,6 @@ const OurStory = () => {
           
        >
         </div>
-      </div>
       </div>
     </>
   );
