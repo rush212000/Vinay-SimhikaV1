a48e750b73be5ba7364ab8fa11aa574419727832
rush212000 <57355106+rush212000@users.noreply.github.com>
2023-07-31 12:58:35 +0100

    Photos

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import Modal from "react-modal"; // Import the react-modal library
+
 import VSfoot from "../img/Photos/vsFoot.png";
 import VS1 from "../img/Photos/V&S1.jpg";
 import VS2 from "../img/Photos/V&S2.jpg";
@@ -12,161 +14,207 @@ import VS9 from "../img/Photos/V&S9.jpg";
 import VS10 from "../img/Photos/V&S10.jpg";
 import VS11 from "../img/Photos/V&S11.jpg";
 import VS12 from "../img/Photos/V&S12.jpg";
-import VS13  from "../img/Photos/V&S13.jpg";
-
-const images = [VS1, VS2, VS3, VS4, VS5, VS6, VS8, VS9, VS10, VS11, VS12, VS13];
-const ImageGallery = ({ onImageClick }) => {
-  const [currentIndex, setCurrentIndex] = useState(null);
-
-  const handleNextImage = () => {
-    const nextIndex = (currentIndex + 1) % images.length;
-    setCurrentIndex(nextIndex);
-  };
-
-  const handlePreviousImage = () => {
-    const previousIndex = (currentIndex - 1 + images.length) % images.length;
-    setCurrentIndex(previousIndex);
-  };
-
-  const handleCloseModal = () => {
-    setCurrentIndex(null);
-  };
-
-  if (currentIndex !== null) {
-    return (
-      <div style={modalStyle}>
-        <div style={arrowStyleLeft} onClick={handlePreviousImage}>
-          &#8249;
-        </div>
-        <div onClick={handleCloseModal}>
-          <img src={images[currentIndex]} alt={`Image ${currentIndex + 1}`} style={modalImageStyle} />
-        </div>
-        <div style={arrowStyleRight} onClick={handleNextImage}>
-          &#8250;
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div style={galleryStyle}>
-      {images.map((image, index) => (
-        <div key={index} style={imageContainerStyle} onClick={() => setCurrentIndex(index)}>
-          <img src={image} alt={`Image ${index + 1}`} style={imageStyle} />
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const Modal = ({ selectedImage, onCloseModal }) => {
-  return (
-    selectedImage && (
-      <div style={modalStyle} onClick={onCloseModal}>
-        <span style={closeBtnStyle}>&times;</span>
-        <div style={centeredImageContainer}>
-          <img src={selectedImage} alt="Selected" style={centeredImageStyle} />
-        </div>
-      </div>
-    )
-  );
-};
+import VS13 from "../img/Photos/V&S13.jpg";
 
 const Photos = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [loadedImages, setLoadedImages] = useState([]);
+  const [images, setImages] = useState([
+    VS1, VS2, VS3, VS4, VS5, VS6, VS7, VS8, VS9, VS10, VS11, VS12, VS13
+  ]);
 
-  useEffect(() => {
-    const importImages = async () => {
-      const loadedImages = await Promise.all(images.map((image) => import(`${image}`).then((module) => module.default)));
-      setLoadedImages(loadedImages);
-    };
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+  const [modalIsOpen, setModalIsOpen] = useState(false); // State for controlling the modal visibility
 
-    importImages();
-  }, []);
-
-  const openModal = (image) => {
+  const handleImageClick = (image) => {
     setSelectedImage(image);
+    setModalIsOpen(true); // Open the modal when an image is clicked
   };
 
   const closeModal = () => {
-    setSelectedImage(null);
+    setModalIsOpen(false); // Close the modal
   };
 
+  const handlePrevImage = () => {
+    const currentIndex = images.indexOf(selectedImage);
+    const prevIndex = (currentIndex - 1 + images.length) % images.length;
+    setSelectedImage(images[prevIndex]);
+  };
+
+  const handleNextImage = () => {
+    const currentIndex = images.indexOf(selectedImage);
+    const nextIndex = (currentIndex + 1) % images.length;
+    setSelectedImage(images[nextIndex]);
+  };
+
+  const styles = `
+  @import url('https://use.typekit.net/upl4fxr.css');
+
+
+
+
+    .image-gallery {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    .thumbnail-container {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 10px;
+      position: relative;
+    }
+
+    .thumbnail-container img {
+      width: 100%;
+      height: auto;
+      max-height: 100px;
+      cursor: pointer;
+      object-fit: cover; /* Maintain aspect ratio */
+    }
+
+    .thumbnail-container img.selected {
+      border: 2px solid #ff5733;
+    }
+
+    .thumbnail-arrows {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      display: ${modalIsOpen ? "flex" : "none"};
+      align-items: center;
+    }
+
+    .thumbnail-arrow {
+      padding: 5px;
+      background-color: rgba(255, 255, 255, 0.8);
+      border: none;
+      cursor: pointer;
+    }
+
+    /* Set 4 square columns for the thumbnails */
+    @media (min-width: 768px) {
+      .thumbnail-container {
+        grid-template-columns: repeat(4, 1fr);
+        gap: 20px;
+      }
+
+      .thumbnail-container img {
+        height: 150px; /* Adjust the height for larger view */
+      }
+    }
+
+    /* Modal Styles */
+    .image-modal {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+    }
+
+    .modal-image-container {
+      position: relative;
+    }
+
+    @import url('https://use.typekit.net/upl4fxr.css');
+    body {
+      font-family: 'Open Sans', sans-serif;
+    }
+    .modal-image {
+      width: 100%;
+      height: auto;
+      max-height: 80vh;
+      max-width: 90vw;
+      object-fit: contain;
+    }
+
+    .modal-controls {
+      position: absolute;
+      top: 50%;
+      left: 0;
+      right: 0;
+      transform: translateY(-50%);
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .modal-control-button {
+      padding: 10px;
+      background-color: rgba(0, 0, 0, 0.8); /* Black background */
+      color: #fff; /* White text */
+      border: none;
+      cursor: pointer;
+    }
+
+    .modal-close-button {
+      position: absolute;
+      top: 15px;
+      right: 10px;
+      padding: 5px;
+      background-color: rgba(0, 0, 0, 0.8);
+      border: none;
+      cursor: pointer;
+    }
+  `;
+
   return (
-    <div style={containerStyle}>
-      <style>
-        {`
-          @import url('https://use.typekit.net/upl4fxr.css');
-          /* Add custom CSS styles for responsive images */
-          .responsive-image {
-            width: 100%;
-            height: auto;
-          }
-          /* Gallery styles */
-          .gallery {
-            font-family: 'shelby', sans-serif;
-            /* Add any other styles you want for the gallery here */
-          }
-        `}
-      </style>
-
-      <style>
-        {`
-          @import url('https://fonts.googleapis.com/css2?family=Cormorant+Infant:ital,wght@0,400;1,400;1,600&display=swap');
-        `}
-      </style>
-
-      <nav style={navbarStyle}>
-        {/* Add your Navbar content here */}
-        {/* For example: */}
-        
+    <div className="image-gallery" style={{ backgroundColor: "#F6F6F6" }}>
+      <style>{styles}</style>
+      <div style={{ backgroundColor: "black", height: "80px" }}></div>
+      <nav style={{ backgroundColor: "#000000", height: "80px" }}>
+        {/* Your navigation bar content */}
       </nav>
-      <div
-      className='container mx-auto h-full flex flex-col'
-        style={{
-          background: '',
-          display: 'flex',
-          flexDirection: 'column', // Change flexDirection to column
-          justifyContent: 'center',
-          alignItems: 'center',
-          position: 'relative',
-          top: -100,
-          padding: "0% 0%",
-        }}
-      >
-      <h3
+
+      <br/>
+      <h2
+        className="min-[0px]:text-[65px] md:text-[75px]"
         style={{
           textAlign: "center",
           fontFamily: "shelby, sans-serif",
           fontStyle: "normal",
-          fontSize: "100px",
           color: "#000000",
         }}
       >
-        <br />
         Photos
-      </h3>
-      </div>
-      <div 
-        style={{ marginTop: '20px', color: 'black',
-              fontWeight: 'regular 400',
-              fontFamily: 'cormorant infant',
-              fontStyle: 'regular',
-              textAlign: 'center',
-              fontSize: '20px',
-              position: 'relative',
-              top: -150,
-              
-    }}>
-     
-          A trip down memory lane...
+      </h2>
+      <br />
+      <div className="thumbnail-container">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Thumbnail ${index + 1}`}
+            className={selectedImage === image ? "selected" : ""}
+            onClick={() => handleImageClick(image)}
+          />
+        ))}
+        <div className="thumbnail-arrows">
+          <button className="thumbnail-arrow" onClick={handlePrevImage}>
+            &lt;
+          </button>
+          <button className="thumbnail-arrow" onClick={handleNextImage}>
+            &gt;
+          </button>
         </div>
-      <div className="gallery"
-      style={{position: 'relative', top: -100}} >
-        <ImageGallery onImageClick={openModal} />
       </div>
-      <Modal selectedImage={selectedImage} onCloseModal={closeModal} />
+
+      {/* Modal */}
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="image-modal">
+        <div className="modal-image-container">
+          <img src={selectedImage} alt="Selected" className="modal-image" />
+          <button className="modal-close-button" onClick={closeModal}>
+            X
+          </button>
+          <div className="modal-controls">
+            <button className="modal-control-button" onClick={handlePrevImage}>
+              &lt;
+            </button>
+            <button className="modal-control-button" onClick={handleNextImage}>
+              &gt;
+            </button>
+          </div>
+        </div>
+      </Modal>
 
       {/* Footer */}
       <footer className="footer mt-auto">
@@ -175,6 +223,7 @@ const Photos = () => {
           <div className="flex justify-center items-center">
             <img src={VSfoot} alt="Footer Image" className="w-20 h-20 mr-4" />
             <p className="text-center text-gray-600">
+              {/* Your footer text here */}
             </p>
           </div>
         </div>
@@ -182,110 +231,5 @@ const Photos = () => {
     </div>
   );
 };
-
-// Styles
-const containerStyle = {
-  display: "relative",
-  flexDirection: "column",
-  alignItems: "center",
-  minHeight: "100vh",
-};
-
-const titleStyle = {
-  fontSize: "24px",
-  fontWeight: "bold",
-  marginBottom: "20px",
-  fontFamily: "Your-Font-Name, sans-serif", // Replace "Your-Font-Name" with the actual font name from Typekit
-};
-
-const galleryStyle = {
-  display: "grid",
-  gridTemplateColumns: "repeat(3, 1fr)", // Display three images in each row
-  gap: "10px",
-  padding: "10px",
-  justifyContent: "center",
-};
-
-const imageContainerStyle = {
-  cursor: "pointer",
-};
-
-const imageStyle = {
-  maxWidth: "100%",
-  height: "auto",
-
-};
-
-const modalStyle = {
-  position: "fixed",
-  top: 1,
-  left: 0,
-  width: "100%",
-  height: "100%",
-  backgroundColor: "rgba(0, 0, 0, 0.7)",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
-
-const closeBtnStyle = {
-  color: "white",
-  fontSize: "30px",
-  position: "relative",
-  top: "20px",
-  right: "20px",
-  cursor: "pointer",
-};
-
-const modalImageStyle = {
-  maxWidth: "50%",
-  maxHeight: "50%",
-  display: "block",
-};
-
-const navbarStyle = {
-  width: "100%",
-  height: "80px",
-  backgroundColor: "black",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  color: "white",
-};
-
-
-
-
-const arrowStyleLeft = {
-  fontSize: "30px",
-  cursor: "pointer",
-  position: "absolute",
-  top: "50%",
-  left: "20px",
-  transform: "translateY(-50%)",
-};
-
-const arrowStyleRight = {
-  fontSize: "30px",
-  cursor: "pointer",
-  position: "absolute",
-  top: "50%",
-  right: "20px",
-  transform: "translateY(-50%)",
-};
-
-const centeredImageContainer = {
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  height: "50%",
-};
-
-const centeredImageStyle = {
-  maxWidth: "50%",
-  maxHeight: "50%",
-  display: "block",
-};
-
 
 export default Photos;
