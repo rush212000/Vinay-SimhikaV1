bd6a3822e62e2dff520ea68d657315d38649063d
rush212000 <57355106+rush212000@users.noreply.github.com>
2023-08-08 16:00:47 +0100

    change the way it looks

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
-import Masonry from "react-masonry-css"; // Import the masonry component
+import Masonry from "react-masonry-css";
 import "./Photos.css";
 
 function Photos() {
   const [current, setCurrent] = useState(null);
   const [images, setImages] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [touchStartX, setTouchStartX] = useState(null);
 
   useEffect(() => {
     const loadImage = async () => {
@@ -36,114 +37,147 @@ function Photos() {
     setCurrent(null);
     setIsModalOpen(false);
   };
+  const handleSwipe = (delta) => {
+    if (Math.abs(delta) > 50) {
+      if (delta > 0 && current !== 0) {
+        setCurrent(current - 1);
+      } else if (delta < 0 && current !== images.length - 1) {
+        setCurrent(current + 1);
+      }
+    }
+  };
 
-  const imagesPerRow = 4; // Number of images per row
+  const handleTouchStart = (e) => {
+    setTouchStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchMove = (e) => {
+    if (touchStartX !== null) {
+      const touchCurrentX = e.touches[0].clientX;
+      const touchDiff = touchCurrentX - touchStartX;
+      handleSwipe(touchDiff);
+    }
+  };
+
+  const handleTouchEnd = () => {
+    setTouchStartX(null);
+  };
+
+  const handleCursorSwipe = (e) => {
+    const cursorDiff = e.deltaX;
+    handleSwipe(cursorDiff);
+  };
+
+  const imagesPerRow = 4;
 
   return (
-
-      <div style={{ backgroundColor: '#F6F6F6' }}>
-           <div style={{ backgroundColor: 'black', height: '80px' }}></div>
-      <nav>
-        {/* Your navigation bar content */}
-      </nav>
-
-    <div>
-    <h1
-      className="min-w-[0px] text-[65px] md:text-[100px]"
-      style={{
-        color: 'Black',
-        textAlign: 'center',
-        fontFamily: 'shelby, sans-serif',
-        fontStyle: 'normal',
-        position: 'relative',
-        bottom: -40,
-      }}
-    >
-      Photos
-    </h1>
-    <div
-      className="min-[0px]:text-[18px] md:text-[20px]"
-      style={{
-        marginTop: '10px',
-        color: 'black',
-        fontWeight: 'regular 400',
-        fontFamily: 'cormorant infant',
-        fontStyle: 'regular',
-        textAlign: 'center',
-      }}
-    >
-      A trip down memory lane...
-    </div>
-    <br />
-    <div className="photos-container">
-      <Masonry
-        breakpointCols={{ default: imagesPerRow, 1100: 3, 700: 2, 500: 1 }} // Responsive columns
-        className="masonry-grid"
-        columnClassName="masonry-grid-column"
-      >
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className="photo-item"
-            onClick={() => openSlideshow(index)}
+    <div style={{ backgroundColor: '#F6F6F6' }}>
+      <div style={{ backgroundColor: 'black', height: '80px' }}></div>
+      <nav>{/* Your navigation bar content */}</nav>
+      <div>
+        <h1
+          className="min-w-[0px] text-[65px] md:text-[100px]"
+          style={{
+            color: 'Black',
+            textAlign: 'center',
+            fontFamily: 'shelby, sans-serif',
+            fontStyle: 'normal',
+            position: 'relative',
+            bottom: -40,
+          }}
+        >
+          Photos
+        </h1>
+        <div
+          className="min-[0px]:text-[18px] md:text-[20px]"
+          style={{
+            marginTop: '10px',
+            color: 'black',
+            fontWeight: 'regular 400',
+            fontFamily: 'cormorant infant',
+            fontStyle: 'regular',
+            textAlign: 'center',
+          }}
+        >
+          A trip down memory lane...
+        </div>
+        <br />
+        <div className="photos-container">
+          <Masonry
+            breakpointCols={{
+              default: imagesPerRow,
+              1100: 3,
+              700: 2,
+              500: 1,
+            }}
+            className="masonry-grid"
+            columnClassName="masonry-grid-column"
           >
-            <img
-              className="photo-image"
-              src={image.src}
-              alt={`Image ${index}`}
-            />
-          </div>
-        ))}
-      </Masonry>
-
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeSlideshow}
-        contentLabel="Image Slideshow"
-        className="modal"
-        overlayClassName="modal_overlay"
-      >
-        {current !== null && (
-          <>
-            <img
-              className="slideshow-image"
-              src={images[current].src}
-              alt={`Image ${current}`}
-            />
-            <div className="slideshow-nav">
-              <button
-                className="slideshow-nav-btn"
-                onClick={() => {
-                  if (current === 0) {
-                    setCurrent(images.length - 1);
-                  } else {
-                    setCurrent(current - 1);
-                  }
-                }}
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className="photo-item"
+                onClick={() => openSlideshow(index)}
+                onTouchStart={handleTouchStart}
+                onTouchEnd={handleTouchEnd}
+                onWheel={handleCursorSwipe}
               >
-                Previous
-              </button>
-              <button
-                className="slideshow-nav-btn"
-                onClick={() => {
-                  if (current === images.length - 1) {
-                    setCurrent(0);
-                  } else {
-                    setCurrent(current + 1);
-                  }
-                }}
-              >
-                Next
-              </button>
-            </div>
-            <div className="modal_close" onClick={closeSlideshow}>
-              &times;
-            </div>
-          </>
-        )}
-      </Modal>
-    </div>
-    </div>
+                <img
+                  className="photo-image"
+                  src={image.src}
+                  alt={`Image ${index}`}
+                />
+              </div>
+            ))}
+          </Masonry>
+          <Modal
+            isOpen={isModalOpen}
+            onRequestClose={closeSlideshow}
+            contentLabel="Image Slideshow"
+            className="modal"
+            overlayClassName="modal_overlay"
+          >
+            {current !== null && (
+              <>
+                <img
+                  className="slideshow-image"
+                  src={images[current].src}
+                  alt={`Image ${current}`}
+                />
+                <div className="slideshow-nav">
+                  <button
+                    className="slideshow-nav-btn"
+                    onClick={() => {
+                      if (current === 0) {
+                        setCurrent(images.length - 1);
+                      } else {
+                        setCurrent(current - 1);
+                      }
+                    }}
+                  >
+                    Previous
+                  </button>
+                  <button
+                    className="slideshow-nav-btn"
+                    onClick={() => {
+                      if (current === images.length - 1) {
+                        setCurrent(0);
+                      } else {
+                        setCurrent(current + 1);
+                      }
+                    }}
+                  >
+                    Next
+                  </button>
+                </div>
+                <div className="modal_close" onClick={closeSlideshow}>
+                  &times;
+                </div>
+              </>
+            )}
+          </Modal>
+        </div>
+      </div>
     </div>
   );
 }
